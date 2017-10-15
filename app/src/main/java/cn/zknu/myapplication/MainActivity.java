46132df27fa987cabb07c404d42e6203f0e3d451
weixijie <3337183289@qq.com>
2017-10-15 12:38:48 +0800

    001

--- app/src/main/java/cn/zknu/myapplication/MainActivity.java
+++ app/src/main/java/cn/zknu/myapplication/MainActivity.java
@@ -10,21 +10,22 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //Person p=new Person
 
-    private EditText etUserName,etPassword;
-    private Button btnLogin,btnCancel;
+
+    private EditText etUserName, etPassword;
+    private Button btnLogin, btnCancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUserName=(EditText)findViewById(R.id.et_user_name);
-        etPassword=(EditText)findViewById(R.id.et_password);
-        btnLogin=(Button)findViewById(R.id.btn_login);
-        btnCancel=(Button)findViewById(R.id.btn_cancel);
+        etUserName = (EditText) findViewById(R.id.et_user_name);
+        etPassword = (EditText) findViewById(R.id.et_password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnCancel = (Button) findViewById(R.id.btn_cancel);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,19 +36,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnCancel.setOnClickListener(this);
     }
 
-    private void login(){
-        String strUserName=etUserName.getText().toString();
-        String strPassword=etPassword.getText().toString();
-        if (strUserName.equals("abc") && strPassword.equals("123")){
-            Toast.makeText(this,"登陆成功！",Toast.LENGTH_LONG).show();//Toast显示提示消息
-        }else{
-            Toast.makeText(this,"用户名或密码错误！",Toast.LENGTH_LONG).show();
+    private void login() {
+        String strUserName = etUserName.getText().toString();
+        String strPassword = etPassword.getText().toString();
+        if (strUserName.equals("abc") && strPassword.equals("123")) {
+            Toast.makeText(this, "登陆成功！", Toast.LENGTH_LONG).show();//Toast显示提示消息
+        } else {
+            Toast.makeText(this, "用户名或密码错误！", Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_login:
                 login();
                 break;
